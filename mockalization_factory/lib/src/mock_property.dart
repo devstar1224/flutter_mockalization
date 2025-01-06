1df26caadbbaf228fce9f3bed80036c9cb10c30a
Sang - Ik Lee <dev_sang@naver.com>
2025-01-06 10:57:32 +0900

    Update mock_property.dart

--- mockalization_factory/lib/src/mock_property.dart
+++ mockalization_factory/lib/src/mock_property.dart
@@ -1,6 +1,12 @@
 class MockProperty {
+  /// String, int 에서만 지원
   final int? length;
+
+  /// String 타입 선언의 DateTime 포멧만 지원중
   final Type? formatType;
 
-  const MockProperty({this.length, this.formatType});
+  const MockProperty({
+    this.length,
+    this.formatType,
+  });
 }
